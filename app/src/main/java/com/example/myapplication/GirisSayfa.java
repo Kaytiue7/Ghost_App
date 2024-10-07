33482222bc6563056c41926a6b6b5a455a54865b
Kaytiue <vlkngms7@gmail.com>
2024-10-07 12:03:40 +0300

    Uçtan uca şifreleme eklendi

--- app/src/main/java/com/example/myapplication/GirisSayfa.java
+++ app/src/main/java/com/example/myapplication/GirisSayfa.java
@@ -14,14 +14,6 @@ import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 
@@ -33,8 +25,6 @@ public class GirisSayfa extends AppCompatActivity {
     private EditText passwordEditText;
     private Button girisButton;
     private TextView kayitOl;
-    private FirebaseAuth auth;
-
     private SharedPreferences sharedPreferences;
     private static final String PREF_NAME = "MyPrefs";
     private static final String KEY_USERNAME = "username";
@@ -45,8 +35,39 @@ public class GirisSayfa extends AppCompatActivity {
         setContentView(R.layout.activity_giris_sayfa);
 
         // Firebase başlatma
-        auth = FirebaseAuth.getInstance();
         db = FirebaseFirestore.getInstance();
+        sharedPreferences = getSharedPreferences(PREF_NAME, Context.MODE_PRIVATE);
+        String storedUsername = sharedPreferences.getString(KEY_USERNAME, null);
+        if (storedUsername != null) {
+            FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+            // Users koleksiyonunda storedUsername ile eşleşen verileri sorgula
+            db.collection("Users")
+                    .whereEqualTo("username", storedUsername) // username alanını kontrol et
+                    .get()
+                    .addOnCompleteListener(task -> {
+                        if (task.isSuccessful()) {
+                            boolean userExists = false; // Kullanıcı mevcut mu kontrolü
+
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                userExists = true; // Eğer bir kullanıcı bulunduysa
+                                break;
+                            }
+
+                            if (userExists) {
+                                // Kullanıcı bulundu, MainActivity'ye yönlendir
+                                navigateToMainActivity();
+                                Toast.makeText(this, "Hoş geldiniz, " + storedUsername, Toast.LENGTH_SHORT).show();
+                            } else {
+                                Toast.makeText(this, "Kullanıcı bulunamadı.", Toast.LENGTH_SHORT).show();
+                            }
+                        } else {
+                            Toast.makeText(this, "Sorgulama hatası: " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    });
+        }
+
+
 
         // UI bileşenlerini bağlama
         usernameEditText = findViewById(R.id.giris_username);
@@ -57,34 +78,16 @@ public class GirisSayfa extends AppCompatActivity {
         // SharedPreferences başlatma
         sharedPreferences = getSharedPreferences(PREF_NAME, Context.MODE_PRIVATE);
 
-        // Yerel veritabanından username verisini al
-        String storedUsername = sharedPreferences.getString(KEY_USERNAME, null);
-
-        if (storedUsername!=null ) {
-            navigateToMainActivity();
-            Toast.makeText(this, "" + storedUsername, Toast.LENGTH_SHORT).show();
-        }
-
-
-
-        Toast.makeText(this, "" + storedUsername, Toast.LENGTH_SHORT).show();
-        kayitOl.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(GirisSayfa.this, KayitOlSayfa.class);
-                startActivity(intent);
-                finish();
-            }
-        });
-
-        // Oturum açma durumu kontrolü
-
-
         // Giriş butonuna tıklama olayı
         girisButton.setOnClickListener(v -> loginUser());
+
+        // Kayıt olma butonuna tıklama olayı
+        kayitOl.setOnClickListener(v -> {
+            Intent intent = new Intent(GirisSayfa.this, KayitOlSayfa.class);
+            startActivity(intent);
+
+        });
     }
-
-
 
     private void loginUser() {
         String username = usernameEditText.getText().toString().trim();
@@ -95,6 +98,7 @@ public class GirisSayfa extends AppCompatActivity {
             return;
         }
 
+        // Kullanıcı adını ve şifreyi veritabanından kontrol et
         db.collection("Users")
                 .get()
                 .addOnCompleteListener(task -> {
@@ -103,16 +107,17 @@ public class GirisSayfa extends AppCompatActivity {
                             String usernamefirebase = document.getString("username");
                             String passwordfirebase = document.getString("password");
 
-                            if (username.equals(usernamefirebase) && password.equals(passwordfirebase)) {
+                            // Şifreyi şifrele
+                            String encryptedPassword = encryptPassword(password);
+
+                            // Giriş kontrolü
+                            if (username.equals(usernamefirebase) && encryptedPassword.equals(passwordfirebase)) {
                                 // Kullanıcı adı yerel veritabanına kaydediliyor
                                 SharedPreferences.Editor editor = sharedPreferences.edit();
                                 editor.putString(KEY_USERNAME, username);
                                 editor.apply();
 
-
                                 navigateToMainActivity();
-
-
                                 return;
                             }
                         }
@@ -127,4 +132,201 @@ public class GirisSayfa extends AppCompatActivity {
         finish();
     }
 
+    private String encryptPassword(String password) {
+        StringBuilder encrypted = new StringBuilder();
+        for (char c : password.toCharArray()) {
+            switch (c) {
+                case 'A':
+                    encrypted.append("1");
+                    break;
+                case 'B':
+                    encrypted.append("!");
+                    break;
+                case 'C':
+                    encrypted.append("Z");
+                    break;
+                case 'D':
+                    encrypted.append("2");
+                    break;
+                case 'E':
+                    encrypted.append("X");
+                    break;
+                case 'F':
+                    encrypted.append("#");
+                    break;
+                case 'G':
+                    encrypted.append("A");
+                    break;
+                case 'H':
+                    encrypted.append("3");
+                    break;
+                case 'I':
+                    encrypted.append("$");
+                    break;
+                case 'J':
+                    encrypted.append("Q");
+                    break;
+                case 'K':
+                    encrypted.append("^");
+                    break;
+                case 'L':
+                    encrypted.append("D");
+                    break;
+                case 'M':
+                    encrypted.append("4");
+                    break;
+                case 'N':
+                    encrypted.append("@");
+                    break;
+                case 'O':
+                    encrypted.append("R");
+                    break;
+                case 'P':
+                    encrypted.append("5");
+                    break;
+                case 'Q':
+                    encrypted.append("T");
+                    break;
+                case 'R':
+                    encrypted.append("6");
+                    break;
+                case 'S':
+                    encrypted.append("*");
+                    break;
+                case 'T':
+                    encrypted.append("7");
+                    break;
+                case 'U':
+                    encrypted.append("E");
+                    break;
+                case 'V':
+                    encrypted.append("8");
+                    break;
+                case 'W':
+                    encrypted.append("Y");
+                    break;
+                case 'X':
+                    encrypted.append("U");
+                    break;
+                case 'Y':
+                    encrypted.append("9");
+                    break;
+                case 'Z':
+                    encrypted.append(")");
+                    break;
+                case 'a':
+                    encrypted.append("0");
+                    break;
+                case 'b':
+                    encrypted.append("?");
+                    break;
+                case 'c':
+                    encrypted.append("W");
+                    break;
+                case 'd':
+                    encrypted.append("!");
+                    break;
+                case 'e':
+                    encrypted.append("J");
+                    break;
+                case 'f':
+                    encrypted.append("5");
+                    break;
+                case 'g':
+                    encrypted.append("H");
+                    break;
+                case 'h':
+                    encrypted.append("6");
+                    break;
+                case 'i':
+                    encrypted.append("^");
+                    break;
+                case 'j':
+                    encrypted.append("K");
+                    break;
+                case 'k':
+                    encrypted.append("M");
+                    break;
+                case 'l':
+                    encrypted.append("S");
+                    break;
+                case 'm':
+                    encrypted.append("8");
+                    break;
+                case 'n':
+                    encrypted.append("Z");
+                    break;
+                case 'o':
+                    encrypted.append("F");
+                    break;
+                case 'p':
+                    encrypted.append("3");
+                    break;
+                case 'q':
+                    encrypted.append("B");
+                    break;
+                case 'r':
+                    encrypted.append("4");
+                    break;
+                case 's':
+                    encrypted.append("V");
+                    break;
+                case 't':
+                    encrypted.append("P");
+                    break;
+                case 'u':
+                    encrypted.append("G");
+                    break;
+                case 'v':
+                    encrypted.append("2");
+                    break;
+                case 'w':
+                    encrypted.append("T");
+                    break;
+                case 'x':
+                    encrypted.append("L");
+                    break;
+                case 'y':
+                    encrypted.append("A");
+                    break;
+                case 'z':
+                    encrypted.append("7");
+                    break;
+                case '0':
+                    encrypted.append("G");
+                    break;
+                case '1':
+                    encrypted.append("H");
+                    break;
+                case '2':
+                    encrypted.append("B");
+                    break;
+                case '3':
+                    encrypted.append("O");
+                    break;
+                case '4':
+                    encrypted.append("J");
+                    break;
+                case '5':
+                    encrypted.append("T");
+                    break;
+                case '6':
+                    encrypted.append("N");
+                    break;
+                case '7':
+                    encrypted.append("E");
+                    break;
+                case '8':
+                    encrypted.append("Y");
+                    break;
+                case '9':
+                    encrypted.append("Q");
+                    break;
+                default:
+                    encrypted.append(c); // Şifreleme dışında kalan karakterler
+                    break;
+            }
+        }
+        return encrypted.toString();
+    }
 }
